9130313e1490b961e0e4fd358183f70d8d4ee266
vadim stelikov <vadim.stelikov@codelibre.lt>
2021-05-11 09:11:45 +0300

    add main Dictionary ( later inject it??)

---
+++ ondato/ondato/Dictionary.cs
@@ -0,0 +1,13 @@
+using ondato.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace ondato
+{
+    public static class DictionaryModel
+    {
+        public static Dictionary<string, DictionaryViewModel> MyDictionary = new Dictionary<string, DictionaryViewModel>();
+
+    }
+}
