cbdb8bd91fc9dd2c585d969304548872237fa04e
vadim stelikov <vadim.stelikov@codelibre.lt>
2021-05-11 09:13:07 +0300

    Dictionary Controller, with Exception example and custom Response object

---
+++ ondato/ondato/Controllers/DictionaryController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ondato.Attributes;
+using ondato.DTO;
+using ondato.Models;
+using ondato.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ondato.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiKeyAuthorize]
+    public class DictionaryController : ControllerBase
+    {
+        private readonly ILogger<DictionaryController> _logger;
+        private readonly IDictionaryService dictionaryService;
+
+        public DictionaryController(ILogger<DictionaryController> logger, IDictionaryService dictionaryService)
+        {
+            _logger = logger;
+            this.dictionaryService = dictionaryService;
+        }
+
+        // GET api/values/5  
+        [HttpGet("key")]
+        public IActionResult Get(string key)
+        {
+            try
+            {
+                return Ok(dictionaryService.GetData(key));
+
+            } catch(Exception ex)
+            {
+                return BadRequest(new Response() { Status = "Error", Error = "Item not Found" });
+            }
+        }
+
+        // POST api/values  
+        [HttpPost]
+        public void Create([FromBody] KeyValueDto yourDto)
+        {
+            dictionaryService.AddData(yourDto.Key, yourDto.Data, yourDto.Valid);
+        }
+
+        // PUT api/values/5  
+        [HttpPut]
+        public void Append(KeyValueDto yourDto)
+        {
+            dictionaryService.UpdateData(yourDto.Key, yourDto.Data);
+        }
+
+        // DELETE api/values/key 
+        [HttpDelete("key")]
+        public void Delete(string key)
+        {
+            dictionaryService.RemoveData(key);
+        }
+    }
+}
---
+++ ondato/ondato/Models/Response.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace ondato.Models {
+  
+    public class Response
+    {
+        public string Data { get; set; }
+        public string Error { get; set; }
+        public string Status { get; set; }
+    }
+}
